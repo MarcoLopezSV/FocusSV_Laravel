b8cadea7eaee5824606889267fb14de8705a29d8
MarcoLopezSV <marco.lopez@catolica.edu.sv>
2022-07-10 08:50:44 -0600

    Created command file that gets the data of the API, base model and migration files

---
+++ app/Console/Commands/getApiData.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Http;
+use App\Models\Population;
+
+class getApiData extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'getapi:data';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Saves all the data of the External API into a variable and returns it on console and sends its to a view';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        //variable to store all the data from api using GET method
+        $data = Http::get("https://datausa.io/api/data?drilldowns=Nation&measures=Population");
+        //return it temporarily into the view decoded as json
+        return view("data", [ "data" => json_decode($data)]);
+        
+    }
+}
