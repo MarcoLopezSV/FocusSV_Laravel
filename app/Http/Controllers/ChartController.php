f835dfa44070ade98ff55a4cd56fc6c4449c857f
MarcoLopezSV <marco.lopez@catolica.edu.sv>
2022-07-10 09:06:10 -0600

    Controllers for the Population and Chart that will be added in the final view

---
+++ app/Http/Controllers/ChartController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Population;
+use Illuminate\Support\Facades\DB;
+
+class ChartController extends Controller
+{
+    //
+}
